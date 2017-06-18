6e837fa339d0ffb11ee870f3945dd33e30017ff1
wondervictor <victorchanchina@gmail.com>
2017-06-19 00:00:39 +0800

    start to test

--- data_provider.py
+++ data_provider.py
@@ -1,2 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import cPickle as pickle
+import numpy as np
+import os
+
+
+
+def data_reader(path, n):
+    def reader():
+        with open(path, 'r') as f:
+            datadict = pickle.load(f)
+            X = datadict['data']
+            Y = datadict['labels']
+            X = X.reshape(10000, 3072).astype("float")
+            Y = np.array(Y)
+
+            size = 10000
+            for i in range(size):
+                yield {'image': X[i] / 255.0,
+                       'label': int(Y[i])
+                       }
+    return reader
--- vgg_net.py
+++ vgg_net.py
@@ -3,6 +3,7 @@
 import paddle.v2 as paddle
 import sys
 import gzip
+import data_provider
 
 # initialize paddle and its trainer
 paddle.init(use_gpu=False, trainer_count=2)
@@ -19,12 +20,11 @@ def event_handler(event):
         # save parameters
         with gzip.open('output/params_pass_%d.tar.gz' % event.pass_id, 'w') as f:
             parameters.to_tar(f)
-
-        # result = trainer.test(
-        #     reader=paddle.batch(
-        #         paddle.dataset.cifar.test10(), batch_size=128),
-        #     feeding=feeding)
-        # print "\nTest with Pass %d, %s" % (event.pass_id, result.metrics)
+        filepath = '/Users/vic/Dev/DeepLearning/Paddle/VGG-CIFAR/Images/cifar-10-batches-py/test_batch'
+        result = trainer.test(
+            reader=paddle.batch(data_provider.data_reader(filepath, 0), batch_size=128),
+            feeding=feeding)
+        print "\nTest with Pass %d, %s" % (event.pass_id, result.metrics)
 
 image_size = 3 * 32 * 32
 
@@ -40,14 +40,14 @@ conv_1 = paddle.layer.img_conv(input=image,
                                filter_size=filter_size,
                                num_channels=num_channels,
                                num_filters=num_filters_1,
-                               stride=1,
+                               stride=1
                                )
 
-conv_2 = paddle.layer.img_conv(conv_1,
+conv_2 = paddle.layer.img_conv(input=conv_1,
                                filter_size=filter_size,
                                num_channels=num_channels,
                                num_filters=num_filters_1,
-                               stride=1,
+                               stride=1
                                )
 
 pool_1 = paddle.layer.img_pool(input=conv_2,
@@ -61,14 +61,14 @@ conv_3 = paddle.layer.img_conv(input=pool_1,
                                filter_size=filter_size,
                                num_channels=num_channels,
                                num_filters=num_filters_1,
-                               stride=1,
+                               stride=1
                                )
 
 conv_4 = paddle.layer.img_conv(input=conv_3,
                                filter_size=filter_size,
                                num_channels=num_channels,
                                num_filters=num_filters_1,
-                               stride=1,
+                               stride=1
                                )
 
 pool_2 = paddle.layer.img_pool(input=conv_4,
@@ -83,7 +83,7 @@ conv_5 = paddle.layer.img_conv(input=pool_2,
                                filter_size=filter_size,
                                num_channels=num_channels,
                                num_filters=num_filters_1,
-                               stride=1,
+                               stride=1
                                )
 
 conv_6 = paddle.layer.img_conv(input=conv_5,
@@ -105,21 +105,21 @@ conv_7 = paddle.layer.img_conv(input=pool_3,
                                filter_size=filter_size,
                                num_channels=num_channels,
                                num_filters=num_filters_1,
-                               stride=1,
+                               stride=1
                                )
 
 conv_8 = paddle.layer.img_conv(input=conv_7,
                                filter_size=filter_size,
                                num_channels=num_channels,
                                num_filters=num_filters_1,
-                               stride=1,
+                               stride=1
                                )
 
 conv_9 = paddle.layer.img_conv(input=conv_8,
                                filter_size=filter_size,
                                num_channels=num_channels,
                                num_filters=num_filters_1,
-                               stride=1,
+                               stride=1
                                )
 
 pool_4 = paddle.layer.img_pool(input=conv_9,
@@ -133,14 +133,14 @@ conv_10 = paddle.layer.img_conv(input=pool_4,
                                 filter_size=filter_size,
                                 num_channels=num_channels,
                                 num_filters=num_filters_1,
-                                stride=1,
+                                stride=1
                                 )
 
 conv_11 = paddle.layer.img_conv(input=conv_10,
                                 filter_size=filter_size,
                                 num_channels=num_channels,
                                 num_filters=num_filters_1,
-                                stride=1,
+                                stride=1
                                 )
 
 conv_12 = paddle.layer.img_conv(input=conv_11,
@@ -158,8 +158,11 @@ pool_5 = paddle.layer.img_pool(input=conv_12,
 
 
 fc_1 = paddle.layer.fc(input=pool_5, size=4096, act=paddle.activation.Sigmoid())
+
 fc_2 = paddle.layer.fc(input=fc_1, size=4096, act=paddle.activation.Sigmoid())
+
 fc_3 = paddle.layer.fc(input=fc_2, size=1000, act=paddle.activation.Sigmoid())
+
 output_layer = paddle.layer.fc(input=fc_3, size=10, act=paddle.activation.Softmax())
 
 label = paddle.layer.data(name='label', type=paddle.data_type.integer_value(10))
@@ -168,8 +171,13 @@ cost = paddle.layer.classification_cost(input=output_layer, label=label)
 
 parameters = paddle.parameters.create(cost)
 
-optimizer = paddle.optimizer.AdaDelta()
-
+optimizer = paddle.optimizer.Momentum(
+    momentum=0.9,
+    regularization=paddle.optimizer.L2Regularization(rate=0.0002 * 128),
+    learning_rate=0.1 / 128.0,
+    learning_rate_decay_a=0.1,
+    learning_rate_decay_b=50000 * 100,
+    learning_rate_schedule='discexp')
 
 trainer = paddle.trainer.SGD(cost=cost,
                              parameters=parameters,
@@ -178,5 +186,6 @@ trainer = paddle.trainer.SGD(cost=cost,
 
 feeding = {'image': 0,
            'label': 1}
-
-trainer.train(num_passes=100, event_handler=event_handler, feeding=feeding)
+file_path = '/Users/vic/Dev/DeepLearning/Paddle/VGG-CIFAR/Images/cifar-10-batches-py/data_batch_1'
+reader = data_provider.data_reader(file_path, 0)
+trainer.train(num_passes=100, reader=paddle.batch(reader, 10000), event_handler=event_handler, feeding=feeding)
